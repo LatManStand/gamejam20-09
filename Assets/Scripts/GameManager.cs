0795d8692e7087135883f7f684b3e395c9315e9c
Raúl Francés <raulfrancesm1997@gmail.com>
2021-09-15 20:05:25 +0200

    MainMenu base

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public SceneLoader instance;
+
+    public bool isSettingsOpen = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void LoadSceneName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadSceneIndex(int number)
+    {
+        SceneManager.LoadScene(number);
+    }
+
+    public void LoadSettings()
+    {
+        if (!isSettingsOpen)
+        {
+            SceneManager.LoadScene(3, LoadSceneMode.Additive);
+            isSettingsOpen = true;
+        }
+        else
+        {
+            UnloadSettings();
+        }
+    }
+
+    public void UnloadSettings()
+    {
+        SceneManager.UnloadSceneAsync(3);
+        isSettingsOpen = false;
+    }
+}
