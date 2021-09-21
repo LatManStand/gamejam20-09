91f5dd858934b8682bacda136383913c32b8a632
Alejandro Gemas <alejandro.gemas@gmail.com>
2021-09-21 20:24:17 +0200

    Added credits scene scripts and animations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    public GameManager instance;
+    public static GameManager instance;
+    private int slot;
+
+    // IMPORTANT DATA
+
+    public string language;
+    public bool isCargarPartida;
+
+
+    public bool hasHorse = false;
+    public bool hasToothbrush = false;
+    public AudioSource audiosrc;
+
+    public Scene lastScene;
 
     private void Awake()
     {
+        Application.targetFrameRate = 300;
+
         if (instance == null)
         {
             instance = this;
@@ -14,5 +32,100 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        isCargarPartida = false;
+    }
+    public void setIsCargarPartida(bool value)
+    {
+        isCargarPartida = value;
+    }
+
+    public bool getIsCargarPartida()
+    {
+        return isCargarPartida;
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveData();
+    }
+
+    //Called to start de game
+    public void StartGame(int _slot)
+    {
+        slot = _slot;
+        // IMPORTANT DATA
+
+        SaveData();
+
+        GameManager.instance.LoadScene("IntroGame");
+    }
+
+    public void QuitGame()
+    {
+        SaveData();
+        Application.Quit();
+    }
+
+    public void LoadScene(string nameLevel)
+    {
+        lastScene = SceneManager.GetActiveScene();
+        if (nameLevel.Equals("Ending") || nameLevel.Equals("Creditos"))
+        {
+            audiosrc.Stop();
+        }
+
+        SceneManager.LoadScene(nameLevel);
+    }
+
+    private IEnumerator LoadAsyncScene(string nameLevel)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nameLevel);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void SaveData()
+    {
+        // SAVE IMPORTANT DATA
+        //PlayerPrefs.SetInt("helpedWolf" + slot, helpedWolf);
+
+        PlayerPrefs.Save();
+    }
+
+    public void setSlot(int _slot)
+    {
+        slot = _slot;
+    }
+
+    public void LoadData(int _slot)
+    {
+        slot = _slot;
+        // helpedWolf = PlayerPrefs.GetInt("helpedWolf" + helpedWolf);
+    }
+
+    public int existeSlotPartidaGuardada(int _slot)
+    {
+        return PlayerPrefs.GetInt("Game" + _slot);
+    }
+
+    public int getDiaPartidaGuardada(int _slot)
+    {
+        return PlayerPrefs.GetInt("Game" + _slot);
+    }
+
+    public void setLanguage(string lg)
+    {
+        this.language = lg;
+        LoadScene("MainMenu");
     }
 }
---
+++ Assets/Scripts/UI/ButtonAnimationMenu.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimationMenu : MonoBehaviour
+{
+    public int slot;
+    public GameObject barra1;
+    public GameObject barra2;
+    public Text texto1;
+
+    private void Start()
+    {
+        if(GameManager.instance.language == "es")
+        {
+            texto1.text = "Nueva Partida";
+        }
+        else
+        {
+            texto1.text = "New slot";
+        }
+
+        barra1.SetActive(false);
+        barra2.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        if (GameManager.instance)
+        {
+            if (GameManager.instance.getIsCargarPartida())
+            { 
+                Debug.Log("IsCargarPartida");
+                if (GameManager.instance.language == "es")
+                {
+                    Debug.Log("IsCargarPartida en es");
+                    texto1.text = "Slot vacío";
+                }
+                else
+                {
+                    Debug.Log("IsCargarPartida en en");
+                    texto1.text = "Empty slot";
+                }
+            }
+            else
+            {
+                if (GameManager.instance.language == "es")
+                {
+                    texto1.text = "Nueva Partida";
+                }
+                else
+                {
+                    texto1.text = "New slot";
+                }
+            }
+        }
+    }
+
+    public void ActionButton()
+    {
+        if (GameManager.instance.getIsCargarPartida())
+        {
+            if (GameManager.instance.existeSlotPartidaGuardada(slot) != 0)
+            {
+                GameManager.instance.LoadData(slot);
+                GameManager.instance.LoadScene("World");
+            }
+        }
+        else
+        {
+            GameManager.instance.StartGame(slot);
+        }
+    }
+
+    public void OnMouseEnter()
+    {
+        barra1.SetActive(true);
+        barra2.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        barra1.SetActive(false);
+        barra2.SetActive(false);
+    }
+}
