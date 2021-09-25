2a269c9ba50b46f8c63dfe5967dfe6a965b9e99d
Alejandro Gemas <agt@mercell.com>
2021-09-25 15:44:17 +0200

    Added all puzzle scenes

--- Assets/Scripts/Game/ClickablePuzzleLevel.cs
+++ Assets/Scripts/Game/ClickablePuzzleLevel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ClickablePuzzleLevel : MonoBehaviour
 {
-    public GameObject obj_selection;
+    //public GameObject obj_selection;
     public GameObject obj_material;
     public string folderPath = "Scenes/Puzzles/Puzzle_";
     public string puzzleNumber;
@@ -12,12 +12,14 @@ public class ClickablePuzzleLevel : MonoBehaviour
 
     private ClickableWorlLevel cWL;
     private Renderer objTextureRender;
+    private Vector3 initialSize;
 
     private void Start()
     {
+        initialSize = this.gameObject.transform.localScale;
         objTextureRender = obj_material.GetComponent<Renderer>();
         cWL = this.gameObject.GetComponentInParent<ClickableWorlLevel>();
-        obj_selection.SetActive(false);
+        //obj_selection.SetActive(false);
         isUnlocked = PlayerPrefs.GetInt("puzzle_" + puzzleNumber) == 1 ? true : false;
         if (!isUnlocked)
         {
@@ -29,7 +31,8 @@ public class ClickablePuzzleLevel : MonoBehaviour
     {
         if (!GameManager.instance.isPause() && cWL.getIsSelected() && isUnlocked)
         {
-            obj_selection.SetActive(true);
+            //obj_selection.SetActive(true);
+            this.gameObject.transform.localScale = new Vector3(initialSize.x + 5, initialSize.y + 5, initialSize.z + 5);
         }
     }
 
@@ -37,7 +40,8 @@ public class ClickablePuzzleLevel : MonoBehaviour
     {
         if (!GameManager.instance.isPause() && cWL.getIsSelected() && isUnlocked)
         {
-            obj_selection.SetActive(false);
+            //obj_selection.SetActive(false);ç
+            this.gameObject.transform.localScale = initialSize;
         }
     }
 
