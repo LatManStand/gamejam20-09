87411f96bf436ef796fa97e1760976f3ef154474
Alejandro Gemas <agt@mercell.com>
2021-09-26 16:25:53 +0200

    Water + map improvement + things

--- Assets/Scripts/Game/MapManager.cs
+++ Assets/Scripts/Game/MapManager.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-
     public bool starTranstion;
     public GameObject MainCamera;
-    public GameObject transion1;
+    public GameObject transion_level_1;
+    public GameObject transion_level_2;
+    public GameObject transion_level_3;
+    public GameObject transion_level_4;
 
+    public GameObject backButton;
     public GameObject dialogueSystem;
+
+    [Space(10)]
+    [Header("Worlds levels")]
+    public ClickableWorlLevel level_1;
+    public ClickableWorlLevel level_2;
+    public ClickableWorlLevel level_3;
+    public ClickableWorlLevel level_4;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +28,11 @@ public class MapManager : MonoBehaviour
         starTranstion = false;
         dialogueSystem.SetActive(false);
         MainCamera.SetActive(true);
-        transion1.SetActive(false);
+        transion_level_1.SetActive(false);
+        transion_level_2.SetActive(false);
+        transion_level_3.SetActive(false);
+        transion_level_4.SetActive(false);
+        backButton.SetActive(false);
     }
 
     private void FixedUpdate()
@@ -36,13 +50,40 @@ public class MapManager : MonoBehaviour
         activateTransition(levelNumber);
     }
 
+    public void GoBackToMap ()
+    {
+        starTranstion = false;
+        transion_level_1.SetActive(false);
+        transion_level_2.SetActive(false);
+        transion_level_3.SetActive(false);
+        transion_level_4.SetActive(false);
+        backButton.SetActive(false);
+        MainCamera.SetActive(true);
+
+        level_1.GoBackToMenu();
+        level_2.GoBackToMenu();
+        level_3.GoBackToMenu();
+        level_4.GoBackToMenu();
+        Debug.Log("Volvimos al mapa!");
+    }
+
     private void activateTransition (int levelNumber)
     {
         MainCamera.SetActive(false);
-        switch(levelNumber)
+        backButton.SetActive(true);
+        switch (levelNumber)
         {
             case 0:
-                transion1.SetActive(true);
+                transion_level_1.SetActive(true);
+                break;
+            case 1:
+                transion_level_2.SetActive(true);
+                break;
+            case 2:
+                transion_level_3.SetActive(true);
+                break;
+            case 3:
+                transion_level_4.SetActive(true);
                 break;
         }
     }
