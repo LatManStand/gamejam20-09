5fd8972b87553c404e039efaef429cc148eef73a
Alejandro Gemas <alejandro.gemas@gmail.com>
2021-09-27 18:16:43 +0200

    Ahora podemos recargar los puzzles

--- Assets/Scripts/Puzzles/PuzzleManagerWithDialogue.cs
+++ Assets/Scripts/Puzzles/PuzzleManagerWithDialogue.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PuzzleManagerWithDialogue : MonoBehaviour
 {
@@ -34,5 +35,10 @@ public class PuzzleManagerWithDialogue : MonoBehaviour
     {
         dialogue.SetActive(true);
         dialogue.GetComponent<DialogueTrigger>().TriggerDialogue();
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
